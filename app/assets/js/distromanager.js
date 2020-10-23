71d02de4635eca42cafbc3e957b36ed4a893aba0
WozeLyrizia <73300844+WozeLyrizia@users.noreply.github.com>
2020-10-23 14:28:07 +0200

    5

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,7 +537,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'http://download1595.mediafire.com/3gp82lhqi0yg/klc3iveu6k77g6a/distribution.json'
+        const distroURL = 'http://lyrizia.atspace.cc/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
